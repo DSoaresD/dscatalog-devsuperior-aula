ecd8164d5771f14b395b867873246fe6fe7d159e
Daniel Soares <danielsoares321@gmail.com>
2022-01-18 17:44:48 -0300

    Custom validation response

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
@@ -4,10 +4,6 @@ import java.io.Serializable;
 import java.time.Instant;
 
 public class StandardError implements Serializable{
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	
