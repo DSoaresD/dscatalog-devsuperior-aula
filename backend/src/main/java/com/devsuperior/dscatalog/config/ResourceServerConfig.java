e32bfb6cc5f30484b0621697ea9340b0d3912008
Daniel Soares <danielsoares321@gmail.com>
2022-01-21 00:44:10 -0300

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,10 +19,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	//primeiro override 2 metodos ResourceServerConfigurerAdapter com o botao direito 
 	
 	@Autowired
+	private Environment env;
+	
+	@Autowired
 	private JwtTokenStore tokenStore;
 	
 	
-	private static final String[] PUBLIC = { "/oauth/token" };	
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };	
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**","/categories/**" };
 	private static final String[] ADMIN = { "/users/**" };
 	
@@ -35,6 +41,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	//define quem pode acessar oque.
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		//liberar acesso ao H2
+		//se nos profiles ativos conter o profile test faça...
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
 		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
